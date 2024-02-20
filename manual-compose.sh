ffbe5a9dcbdf9ed8074487968487cadb2da5c164
parc02 <pascalmin49@gmail.com>
2024-02-20 11:47:21 +0900

    update manual-compose.sh (blog)

--- manual-compose.sh
+++ manual-compose.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+#rm&rmi
+figlet flush container & Images
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker rmi -f $(sudo docker images -q)
+
+#BUILD    
+figlet BUILD
 sudo docker build -t blog-a -f cron-1/Dockerfile cron-1
+sudo docker build -t blog-b -f cron-2/Dockerfile cron-2
+sudo docker build -t blog-lb -f lb/Dockerfile lb
+
+#RUN
+figlet RUN
 sudo docker run -d --name blog-a-1 -p 8001:80 blog-a
+sudo docker run -d --name blog-b-1 -p 8002:80 blog-a
+sudo docker run -d --name blog-lb-1 -p 8003:80 blog-lb
 
-sl
+#DOCKER NETWORK
+figlet NETWORK
+sudo docker network rm blog-123
+sudo docker network create blog-123
+sudo docker network connect blog-123 blog-a-1
+sudo docker network connect blog-123 blog-b-1
+sudo docker network connect blog-123 blog-lb-1
 
+figlet PS
 sudo docker ps
+
+sl -alF
